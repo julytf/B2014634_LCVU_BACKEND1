40079e75841f060a4823f44eed450aa46f40467c
Ám Vương <julytf.liyc@gmail.com>
2023-10-04 14:59:54 +0700

    wip

--- contactbook-backend/app.js
+++ contactbook-backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
@@ -15,17 +17,12 @@ app.get("/", (req, res) => {
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res, next) => {
-  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
   return next(new ApiError(404, "Resource not found"));
 });
 
 app.use((err, req, res, next) => {
-  // Middleware xử lý lỗi tập trung.
-  // Trong các đoạn code xử lý ở các route, gọi next(error)
-  // sẽ chuyển về middleware xử lý lỗi này
-  return res.status(error.statusCode || 500).json({
-    message: error.message || "Internal Server Error",
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
   });
 });
 
---
+++ contactbook-backend/app/services/contact.service.js
@@ -0,0 +1,59 @@
+const { ObjectId } = require("mongodb");
+
+class ContactService {
+  constructor(client) {
+    this.Contact = client.db().collection("contacts");
+  }
+  extractConactData(payload) {
+    const contact = {
+      name: payload.name,
+      email: payload.email,
+      address: payload.address,
+      phone: payload.phone,
+      favorite: payload.favorite,
+    };
+    // Remove undefined fields
+    Object.keys(contact).forEach(
+      (key) => contact[key] === undefined && delete contact[key]
+    );
+    return contact;
+  }
+  async create(payload) {
+    const contact = this.extractConactData(payload);
+    const result = await this.Contact.findOneAndUpdate(
+      contact,
+      { $set: { favorite: contact.favorite === true } },
+      { returnDocument: "after", upsert: true }
+    );
+    return result;
+  }
+  async find(filter) {
+    const cursor = await this.Contact.find(filter);
+    return await cursor.toArray();
+  }
+  async findsyName(name) {
+    return await this.find({
+      name: { Sregex: new RegExp(name), Soptions: "¡" },
+    });
+  }
+  async findById(id) {
+    return await this.Contact.findOne({
+      _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+    });
+  }
+  async update(id, payload) {
+    const filter = {
+      _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+    };
+    const update = this.extractConactData(payload);
+    const result = await this.Contact.findOneAndUpdate(
+      filter,
+      { $set: update },
+      { returnDocument: "after" }
+    );
+    console.log(result);
+    return result.value;
+  }
+}
+
+module.exports = ContactService;
